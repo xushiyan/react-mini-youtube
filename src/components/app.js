2dd639538d3e2e8628d3cdfe236c5ea019e30110
Xu Shiyan <xushiyan.1990@gmail.com>
2018-02-11 20:39:43 +0800

    Make video list clickable and load on search

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,47 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import SearchBar from './components/search_bar';
+import SearchBar from './search_bar';
+import VideoList from './video_list';
+import VideoDetail from './video_detail';
+import YTSearch from 'youtube-api-search';
 
-const API_KEY = 'AIzaSyDopZpl1-aBFAnktFHvvIgrnvf84lB2su0';
+const API_KEY = 'AIzaSyC-SKUIra4ZqeJL8uJwBTk-Ppr1E8Has-Q';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <SearchBar />
-      </div>
-    );
-  }
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('game of thrones');
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, videos => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce(term => {this.videoSearch(term)}, 1000);
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+
 }
+
+export default App;
