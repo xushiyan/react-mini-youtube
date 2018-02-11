760b6885bb3d278a35ce5b1f49020e2b0da27a10
Xu Shiyan <xushiyan.1990@gmail.com>
2018-02-11 18:45:06 +0800

    Add react components

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDopZpl1-aBFAnktFHvvIgrnvf84lB2su0';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
